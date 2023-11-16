9ec8206852711907da82de2646dbf917ebe13310
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-11-16 15:05:07 -0800

    Project Commit 1

--- func_1.c
+++ func_1.c
@@ -63,7 +63,11 @@ void print_stack(stack_t **stack, unsigned int line)
 void print_stack_top(stack_t **stack, unsigned int line)
 {
 	if (stack == NULL || *stack == NULL)
+	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line);
+		free_all_node();
+		exit(EXIT_FAILURE);
+	}
 
 	printf("%d\n", (*stack)->n);
 }
